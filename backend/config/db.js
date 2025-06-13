77ccd69e60c32854ab84554e1e47157980b19a0d
Pierre-Simon <117252760+Pratik9113@users.noreply.github.com>
2025-06-13 14:00:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 export const connectDB = async () =>{
     try {
-        await mongoose.connect(`mongodb+srv://pratikpatil9113:Pratik%406878@cluster0.fxavz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://pratikpatil9113:ysTFY94aaE367Xig@cluster0.38kshrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("db connected");
     } catch (error) {
         console.error("error ", error);
